5488593ac421aed03e0e990e7c96ac49cb58d329
gaussuddin <114355359+gaussuddin@users.noreply.github.com>
2025-06-04 14:27:45 +0600

    Update ticket_bot.py

--- ticket_bot.py
+++ ticket_bot.py
@@ -13,11 +13,12 @@ bot = Bot(token=BOT_TOKEN)
 
 # === Editable Section ===
 ROUTES = [
-    ("Rajshahi", "Dhaka"),
-    ("Chapainawabganj", "Dhaka")
+    ("Dhaka", "Rajshahi"),
+    ("Biman_Bandar", "Rajshahi"),
+    ("Dhaka", "Chapainawabganj")
 ]
 
-DATES = ["05-Jun-2025", "06-Jun-2025"]
+DATES = ["04-Jun-2025", "05-Jun-2025"]
 TRAVEL_CLASS = "S_CHAIR"
 
 # === Do Not Edit Below This Line ===
